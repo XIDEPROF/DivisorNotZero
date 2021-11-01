3a9d1b431e83cc29a86a5faebae9de79030ac329
Serge <78102903+MrXides@users.noreply.github.com>
2021-11-01 16:49:26 +0300

    T-01
    DivisorNotZero

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	double i=50,j=150,d;
+	if (i != 0){
+	    System.out.println("Делитель не равен 0");
+	    d=j/i;
+        System.out.println("J/I = "+ d);
+    }
+    }
+}
